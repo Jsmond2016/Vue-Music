0fc1bb0f630ea3ecec4746862fff6a84ec11a68e
jsmond2016 <jsmond2016@gmail.com>
2018-04-04 18:01:43 +0800

    finish m-header module

--- src/main.js
+++ src/main.js
@@ -6,16 +6,11 @@ import fastclick from 'fastclick'
 import 'common/stylus/index.styl'
 /* eslint-disable */
 
-
 Vue.config.productionTip = false
-
 
 fastclick.attach(document.body)
 
-
-
 new Vue({
   el: '#app',
-  router,
   render: h => h(App)
 })
