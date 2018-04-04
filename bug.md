22c27609479be24c3461c2e47b006318df498de8
jsmond2016 <jsmond2016@gmail.com>
2018-04-04 17:31:56 +0800

    add header css

---
+++ bug.md
@@ -0,0 +1,7 @@
+# vue-music  开发遇到bug
+
+- `ESLint` 换行警告报错
+
+  ![](https://i.loli.net/2018/04/04/5ac48434a971c.png)
+
+解决办法: [[翻译] ESLint的配置 (二)](https://segmentfault.com/a/1190000009845435)
---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="m-header">
+    <div class="icon"></div>
+    <h1 class="text">Chicken Music</h1>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export  default {}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylu/minxin"
+
+  .m-header
+    position: relative
+</style>
