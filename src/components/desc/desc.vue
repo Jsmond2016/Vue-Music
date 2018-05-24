673e10399ecfc8e602061054741c00d69aa5c14d
jsmond2016 <jsmond2016@gmail.com>
2018-05-24 23:53:10 +0800

    添加 desc组件

---
+++ src/components/desc/desc.vue
@@ -0,0 +1,22 @@
+<template>
+    <transition name="slide">
+      <music-list></music-list>
+    </transition>
+</template>
+
+<script type="text/ecmascript-6">
+  import MusicList from 'common/music-list/music-list'
+    export default {
+    components: {
+      MusicList
+    }
+    }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+   .slide-enter-active, .slide-leave-active
+    transition: all 0.3s
+
+  .slide-enter, .slide-leave-to
+    transform: translate3d(100%, 0, 0)
+</style>
