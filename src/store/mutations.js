684a1bf6eab6b175c7dc3f0d4e9bfbe88c1233c9
黄锦 <jin.huang02@hand-china.com>
2020-04-21 22:05:26 +0800

    fix(vue-music): 修复 vuex 报错问题

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -13,10 +13,13 @@ const mutations = {
     state.fullScreen = flag
   },
   [types.SET_PLAYLIST](state, list) {
-    state.playlist = list
+    // state.playlist = list
+    // state.playlist = JSON.parse(JSON.stringify(list))
+    state.playlist = Object.assign([], list)
   },
   [types.SET_SEQUENCE_LIST](state, list) {
-    state.sequenceList = list
+    // state.sequenceList = list
+    state.sequenceList = Object.assign([], list)
   },
   [types.SET_PLAY_MODE](state, mode) {
     state.mode = mode
@@ -28,16 +31,20 @@ const mutations = {
     state.disc = disc
   },
   [types.SET_TOP_LIST](state, topList) {
-    state.topList = topList
+    // state.topList = topList
+    state.topList = Object.assign([], topList)
   },
   [types.SET_SEARCH_HISTORY](state, history) {
-    state.searchHistory = history
+    // state.searchHistory = history
+    state.playHistory = Object.assign([], history)
   },
   [types.SET_PLAY_HISTORY](state, history) {
-    state.playHistory = history
+    // state.playHistory = history
+    state.playHistory = Object.assign([], history)
   },
   [types.SET_FAVORITE_LIST](state, list) {
-    state.favoriteList = list
+    // state.favoriteList = list
+    state.favoriteList = Object.assign([], list)
   }
 }
 
